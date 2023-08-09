a0e1d8f6491a028637c690213e18f8c710b1048e
Zesan Saha <41072153+jisan66@users.noreply.github.com>
2023-08-09 23:08:00 +0600

    Update Profile Screen

---
+++ lib/ui/screens/update_profile_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UpdateProfileScreen extends StatefulWidget {
+  const UpdateProfileScreen({super.key});
+
+  @override
+  State<UpdateProfileScreen> createState() => _UpdateProfileScreenState();
+}
+
+class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
